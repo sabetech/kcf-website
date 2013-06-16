9a21cfab685b65bf9e2051c3a7985b0ff8293c56
enkansah <emmanuel.nkansah@me.com>
2013-06-16 22:28:04 +0000

    First Template
    A simple template with our discussion at Elysia's house as the basis.

---
+++ template/index.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<!-- metas -->
+    <meta charset="utf-8">
+    <title>KCF &middot; The Official Website of The Kingdom Christian Fellowship</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The Official Website of The Kingdom Christian Fellowship">
+	<meta name="keywords" content="KCF, Christian, Fellowship, Ashesi">
+    <meta name="author" content="Emmanuel Antwi Nkansah">
+
+    <!-- Styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet"->
+ 	<link href="fontawesome/css/font-awesome.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">logo goes here</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Ministries</a></li>
+			  <li><a href="#contact">Events</a></li>
+			  <li><a href="#contact">Gallery</a></li>
+			  <li class="dropdown">
+			                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Extra <b class="caret"></b></a>
+			                <ul class="dropdown-menu">
+			                  <li><a href="#">Prayer Requests</a></li>
+			                  <li><a href="#">Testimonies</a></li>
+			                  <li><a href="#">Alumni</a></li>
+			                </ul>
+			              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    
+
+	<div class="container-narrow">
+	   <div class="row-fluid">
+			<ul class="breadcrumb drop-shadow lifted">
+			  <li><a href="#">Home</a></li>
+			</ul>
+		</div>
+      <!-- Main carousel -->
+      <div id="kcfCarousel" class="carousel slide">
+	  <!-- Carousel items -->
+	  <div class="carousel-inner">
+	    <div class="item active">
+			<img src="assets/img/carousel/slide-1.jpg" alt="">
+		    <div class="carousel-caption">
+		    	<h4>Raising A New Generation of Leaders</h4>
+		        <p>Revealing the Kingdom of God through worship!!</p>
+			</div>
+		</div>
+	    <div class="item">
+	    	<img src="assets/img/carousel/slide-2.jpg" alt="">
+			<div class="carousel-caption">
+				<h4>Worship Night</h4>
+			    <p>Revealing the Kingdom of God through worship!!</p>
+	    	</div>
+	  	</div>
+	  <!-- Carousel nav -->
+		</div>
+	  <a class="carousel-control left" href="#kcfCarousel" data-slide="prev">&lsaquo;</a>
+	  <a class="carousel-control right" href="#kcfCarousel" data-slide="next">&rsaquo;</a>
+	</div>
+
+	<!-- row for mission -->
+	<div class="row-fluid">
+		<div class="well drop-shadow lifted">
+			<h4 align="center" class="text-info">Mission</h4>
+			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		</div>
+	</div>
+	
+      <!-- row of columns -->
+      <div class="row-fluid">
+        <div class="span4 well drop-shadow lifted">
+          <h4 align="center" class="text-info">News</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        </div>
+        <div class="span4 well drop-shadow lifted">
+          <h4 align="center" class="text-info">Ministries</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+       </div>
+        <div class="span4 well drop-shadow lifted">
+          <h4 align="center" class="text-info">Testimonies</h4>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p><span class="pull-left">&copy; Kingdom Christian Fellowship 2013</span><span class="pull-right">Powered by <a href="#">JellyCore llc</a></span></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap-transition.js"></script>
+    <script src="bootstrap/js/bootstrap-alert.js"></script>
+    <script src="bootstrap/js/bootstrap-modal.js"></script>
+    <script src="bootstrap/js/bootstrap-dropdown.js"></script>
+    <script src="bootstrap/js/bootstrap-scrollspy.js"></script>
+    <script src="bootstrap/js/bootstrap-tab.js"></script>
+    <script src="bootstrap/js/bootstrap-tooltip.js"></script>
+    <script src="bootstrap/js/bootstrap-popover.js"></script>
+    <script src="bootstrap/js/bootstrap-button.js"></script>
+    <script src="bootstrap/js/bootstrap-collapse.js"></script>
+    <script src="bootstrap/js/bootstrap-carousel.js"></script>
+    <script src="bootstrap/js/bootstrap-typeahead.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('.carousel').carousel({
+				interval:5000	
+			});
+		});
+	</script>
+  </body>
+</html>
